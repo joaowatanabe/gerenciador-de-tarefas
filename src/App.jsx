99926861985db750181eb9eaa26d6471e634066f
wabemusic <jvwatanabe@gmail.com>
2024-10-22 20:28:40 -0300

    litte style

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import Tasks from "./components/tasks";
 
 function App() {
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <Tasks />
-      <AddTask />
+    <div className="w-screen h-screen bg-cyan-800 flex justify-center p-6">
+      <div className="w-[500px]">
+        <h1 className="text-3xl text-slate-100 font-bold text-center">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask />
+        <Tasks />
+      </div>
     </div>
   );
 }
