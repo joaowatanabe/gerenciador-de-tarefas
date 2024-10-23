58e40d0a0e4008f3752d17019d4cc48f0dbd4aab
wabemusic <jvwatanabe@gmail.com>
2024-10-23 16:23:49 -0300

    AddTask structure and style

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,21 @@
 function AddTask() {
-  return <h1>Add task</h1>;
+  return (
+    <div className="bg-slate-200 p-6 space-y-4 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o sua tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+      />
+      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
