e7b91b2c6e278135cb166b09de28485763c84e38
wabemusic <jvwatanabe@gmail.com>
2024-10-22 23:03:57 -0300

    style tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,43 @@
+import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/tasks";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um DEV full-stack.",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      title: "Estudar react",
+      description: "Estudar react para se aperfeiçoar.",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      title: "Revisar Back-end",
+      description: "Revisar Back-end para se aperfeiçoar.",
+      isCompleted: false,
+    },
+  ]);
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      //preciso atualizar a tarefa
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+
+      // Não preciso atualizar essa tarefa
+      return task;
+    });
+
+    setTasks(newTasks);
+  }
+
   return (
     <div className="w-screen h-screen bg-cyan-800 flex justify-center p-6">
       <div className="w-[500px]">
@@ -9,7 +45,7 @@ function App() {
           Gerenciador de Tarefas
         </h1>
         <AddTask />
-        <Tasks />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
       </div>
     </div>
   );
