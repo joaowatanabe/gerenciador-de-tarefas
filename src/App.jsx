be1d8d8d74a2c1366d0c4965a81d3b363dd3bd42
wabemusic <jvwatanabe@gmail.com>
2024-10-23 16:49:56 -0300

    AddTask complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/tasks";
+import { v4 } from "uuid";
 
 function App() {
   const [tasks, setTasks] = useState([
@@ -43,13 +44,24 @@ function App() {
     setTasks(newTasks);
   }
 
+  function onAddTaskSubmit(title, description) {
+    const newTask = {
+      id: v4(),
+      title,
+      description,
+      isCompleted: false,
+    };
+
+    setTasks([...tasks, newTask]);
+  }
+
   return (
-    <div className="w-screen h-screen bg-cyan-800 flex justify-center p-6">
-      <div className="w-[500px]">
+    <div className="w-screen h-screen bg-emerald-900 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask />
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
           onTaskClick={onTaskClick}
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,38 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="bg-slate-200 p-6 space-y-4 rounded-md shadow flex flex-col">
       <input
         type="text"
         placeholder="Digite o sua tarefa"
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
       />
       <input
         type="text"
         placeholder="Digite a descrição da tarefa"
         className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
       />
-      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+      <button
+        onClick={() => {
+          //verificar se os campos estão preenchidos
+          if(!title.trim() || !description.trim()) {
+            return alert("Preencha os campos da tarefa")
+          }
+
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("")
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
         Adicionar
       </button>
     </div>
