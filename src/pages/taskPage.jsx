7042dfa45126f563c9a046e428006c29a234c820
wabemusic <jvwatanabe@gmail.com>
2024-10-24 00:34:59 -0300

    navigate -1

--- src/pages/taskPage.jsx
+++ src/pages/taskPage.jsx
@@ -1,13 +1,33 @@
-import { useSearchParams } from "react-router-dom";
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
-    <div className="h-screen w-screen bg-emerald-900 p-6">
-      <h1>{title}</h1>
-      <p>{description}</p>
+    <div className="h-screen w-screen bg-emerald-900 p-6 flex">
+      <div className="w-[500px] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
+            <ChevronLeftIcon />
+          </button>
+          <h1 className="text-3xl text-slate-100 font-bold text-center">
+            Detalhes da Tarefa
+          </h1>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md space-y-4">
+          <h2 className="text-xl text-slate-800 font-bold text-center">
+            {title}
+          </h2>
+          <p className="text-slate-800">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
